d49d2a963eb8bb7f2b2168424fe43e0aca13933a
serycjon <jonas.serych@gmail.com>
2021-12-03 10:16:53 +0100

    add more reddit ideas

--- 02.lisp
+++ 02.lisp
@@ -6,14 +6,6 @@
 (defun read-input-d2 (path)
   (mapcar #'parse-line-d2 (uiop:read-file-lines path)))
 
-;; interesting parser from reddit
-;; https://www.reddit.com/r/adventofcode/comments/r6zd93/2021_day_2_solutions/hmwc2nu/
-;; (defun parse (stream)
-;;   (iterate (for line :in-stream stream :using #'read-line)
-;;            (ppcre:register-groups-bind ((#'ensure-keyword command) (#'parse-integer n))
-;;                ("(\\w+) (\\d+)" line)
-;; 	       (collect (cons command n)))))
-
 (defparameter in2 (read-input-d2 #P"./inputs/02.txt"))
 
 (defun d2-p1 ()
@@ -43,3 +35,35 @@
 
 ;; (d2-p1)
 ;; (d2-p2)
+
+;; interesting parser from reddit
+;; https://www.reddit.com/r/adventofcode/comments/r6zd93/2021_day_2_solutions/hmwc2nu/
+;; (defun parse (stream)
+;;   (iterate (for line :in-stream stream :using #'read-line)
+;;            (ppcre:register-groups-bind ((#'ensure-keyword command) (#'parse-integer n))
+;;                ("(\\w+) (\\d+)" line)
+;; 	       (collect (cons command n)))))
+
+
+;; another interesting approach:
+;; https://www.reddit.com/r/adventofcode/comments/r6zd93/2021_day_2_solutions/hmx50fj/
+;; (ql:quickload :parseq)
+
+;; (parseq:defrule up () "up" (:constant '(0 -1)))
+;; (parseq:defrule down () "down" (:constant '(0 1)))
+;; (parseq:defrule forward () "forward" (:constant '(1 0)))
+;; (parseq:defrule value () (rep (0 10) digit) (:string) (:function #'parse-integer))
+;; (parseq:defrule move () (and (or up down forward) " " value) (:lambda (m i s) (mapcar (lambda (v) (* v s)) m)))
+
+;; (print (reduce #'* (reduce (lambda (x y) (mapcar #'+ x y))
+;;                            (mapcar (lambda (line) (parseq:parseq 'move line))
+;;                                    (uiop:read-file-lines "02.input")))))
+
+;; (print (let ((x 0) (y 0) (aim 0))
+;;          (mapc (lambda (line)
+;;                  (let ((movement (parseq:parseq 'move line)))
+;;                    (setf aim (+ aim (cadr movement)))
+;;                    (setf x (+ x (car movement)))
+;;                    (setf y (+ y (* aim (car movement))))))
+;;                (uiop:read-file-lines "02.input"))
+;;          (* x y)))
